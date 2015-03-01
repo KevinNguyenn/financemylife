6def32c67bd75b200cf445cede3e29e9a7d32548
KevinNguyenn <kevinnguyen@KevinNguyenn.local>
2015-03-01 13:06:02 -0500

    l

--- index.php
+++ index.php
@@ -2,23 +2,20 @@
 <html lang="en">
 <head>
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
 	<link type="text/css" rel="stylesheet" href="css/bootstrap-theme.css">
-
-	<meta http-equiv="Content-type" content="text/html; charset=utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-
-	<title>FinanceMyLife</title>
+	<title>FML</title>
 </head>
 
-<body>
+<body>	
 	<div class="first_div">
-		<div id="header">
+		<div id="header">	
 			<div class="logo">
 				<p>FinanceMyLife</p>
 			</div>
 		</div>
 		<div class="main_title">
-			<h1>LET'S FINDANCE YOUR LIFE</h1>
+			<h1>LET'S FIND-ANCE YOUR LIFE</h1>
 		</div>
 		<div class="main_title_sub_title">
 			<p>A simple after graduation life planner.</p>
@@ -30,74 +27,97 @@
 		</div>
 	</div>
 	<div class="info_intro_text">
-			<p>With three simple questions FinanceMyLife can help you in your transition from college to the real world.</p>
+			<p>With three simple questions FinanceMyLife can help you in<br>your transition from college to the real world.</p>
 	</div>
 
 	<div class="info_div">
-		<div class="info_box info_1">
-			<p>hello world</p>
-		</div>
-		<div class="info_box info_2"></div>
-		<div class="info_box info_3"></div>
+		<div class="info_box" id="info_1"></div>
+		<div class="info_box" id="info_2"></div>
+		<div class="info_box" id="info_3"></div>
 	</div>
-
-
 
 	<div class="questionare_block">
 		<div class="questionare_block_field">
-				<div class="input_question">City:</div>
-				<form action="action_page.php" method="get">
-					<input type="text" placeholder="City you want to live in." id="city_textfield">
-				<br>
-				<div class="input_question">State:</div>
-					<input type="text" placeholder="State you want to live in." id="state_textfield">
-				<br>
-				<div class="input_question">Job:</div>
-					<input type="text" placeholder="Job you're looking to apply to." id="job_textfield">
-				<br>
-				<div class="input_question">Debt:</div>
-					<input type="text" placeholder="School and other debts" id="debt_textfield">
+			<form>
 				<br><br>
-
-				<button id="plan_my_life" onclick="presentLifePlan()">Submit</button>
-			</form>
+				<div class="input_question">City:</div>
+					<input type="text" name="firstname" placeholder="City you want to live in." class="input_field" id="city_texfield">
+				<br><br>
+				<div class="input_question">State:</div>
+					<input type="text" name="lastname" placeholder="State you want to live in." class="input_field" id="state_texfield">
+				<br><br>
+				<div class="input_question">Job:</div>
+					<input type="text" name="firstname" placeholder="Job you're looking to apply to." class="input_field" id="job_texfield">
+				<br><br>
+				<div class="input_question">Debt:</div>
+					<input type="text" name="firstname" placeholder="School and other debts" class="input_field" id="debt_texfield">
+				<br><br>
+		</form>
+		</div>
+		<div id="pos_submit" href="#">
+			<div id="submit">
+				<h3>Submit</h3>
+			</div>
 		</div>
 	</div>
 
+	<div class="output">
+		<div id="output_1">
+			<h3>Housing</h3>
+			<ul class="list_things">
+				<li>Mortgage and Principal: <span id="results">$13,440 per year</span></li><br>
+				<li>Property Tax: <span id="results">$4,656</span></li><br>
+				<li>Utilities: <span id="results">$1,932</span></li><br>
+				<li>Total Cost of Housing: <span id="results">$20,028</span></li><br>
+			</ul>
+		</div>
+		<div id="output_2">
+			<h3>Standard of Living</h3>
+			<ul class="list_things">
+				<li>Comapred to San Francisco...<span id="results">Groceries are 22% cheaper</span></li><br>
+				<li>Compared to San Francisco...<span id="results">Housing is 303% more cheaper</span></li><br>
+				<li>Compared to Austin...<span id="results">Groceries are 15% more expensive</span></li><br>
+				<li>Compared to Austin...<span id="results">Transportation is 4% more expensive</span></li><br>
+			</ul>
+		</div>
+		<div id="output_3">
+			<h3>Debt</h3>
+			<ul class="list_things">
+				<li>With a 15 year with a 4.71% interest rate...<span id="results">You pay $1,168 towards your debt</span></li><br>
+			</ul>
+		</div>		
+		<div id="output_4">
+			<h3>Net Income</h3>
+			<ul class="list_things">
+				<li>Gross Income:<span id="results">$69,000</span></li><br>
+				<li>Housing Payments:<span id="results">$20,028</span></li><br>
+				<li>Debt Payments:<span id="results"></span>$1,861</li><br>
+				<li>Total Deductions:<span id="results"></span>$21,889</li><br>
+				<li>Total Cost of Living:<span id="results"></span>$47,111</li><br>
+			</ul>
+		</div>		
+	</div>
+	<div class="bottom">ddd
+	</div>
 
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 	<script>
 		function scrollToAnchor(aid){
 	    	var aTag = $("div[class='"+ aid +"']");
 	    	$('html,body').animate({scrollTop: aTag.offset().top},'slow');
 		}
 		$("#link").click(function() {
-	   		scrollToAnchor('questionare_block');
+	   		scrollToAnchor('info_intro_text');
+		});
+		$("#pos_submit").click(function() {
+	   		scrollToAnchor('output');
 		});
 
-		function presentLifePlan() {
-			echo "Yo";
-			$("#plan_my_life").click(function(event) {
-				event.preventDefault();
-				$(".alert").hide();
-					$.get("action_page.php?city_textfield="+$("#city_textfield").val()+"&state_textfield="+$("#state_textfield").val()+"&job_textfield="+$("#job_textfield").val()+"&debt_textfield="+$("#debt_textfield").val(), function(data) {
-
-
-						if(data =="") {
-							$("#fail").fadeIn();
-						}
-						else {
-							$("#success").html(data).fadeIn();
-						}
-				}
-				else {
-					$
-					$("#noCity").fadeIn();
-				}
-
-			})
-		}
 	</script>
 
 </body>
 </html>
+
+
+
+
+
